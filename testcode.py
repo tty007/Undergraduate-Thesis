c26bc16e4a3911844c2e5c765797406f4398b9be
tty007 <c7dc007@gmail.com>
2019-11-06 19:36:39 +0900

    run testcode/ダウンサンプリング成功

--- testcode.py
+++ testcode.py
@@ -1,6 +1,10 @@
-import datetime
+import pandas as pd
+import time
 
-while True:
-    time = datetime.datetime.now().strftime('%H:%M:%S:%f')
-    datetime = dict(nitiji=time)
-    print(datetime)
+df = pd.read_csv('csv_data/cpu_0.csv')
+
+df['time'] = pd.to_datetime(df['time'], format='%H:%M:%S:%f')
+
+df = df.set_index('time')
+ret = df.resample('100L').mean()
+print(ret)
