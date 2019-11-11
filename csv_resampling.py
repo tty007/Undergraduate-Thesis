0e23a1bef425481ce56bf8c479f2e7a4d85de659
tty007 <c7dc007@gmail.com>
2019-11-11 14:48:57 +0900

    comment-off csv_sampling.csv

--- csv_resampling.py
+++ csv_resampling.py
@@ -20,11 +20,6 @@ def resample_csv(cpu_id):
     diff_df.index.names = ['time[ms]']
     final_df = diff_df
     final_df.to_csv('csv_resample_data/cpu_' + str(cpu_id) + '.csv')
-    # df = pd.read_csv('csv_data/cpu_'+ str(cpu_id) + '.csv')
-    # df['time'] = pd.to_datetime(df['time'], format='%H:%M:%S:%f')
-    # df = df.set_index('time')
-    # new_df = df.resample('100L').mean()
-    # new_df.to_csv('csv_resample_data/cpu_' + str(cpu_id) + '.csv')
 
 resample_csv(1)
 resample_csv(2)
