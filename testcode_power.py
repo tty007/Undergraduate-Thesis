9d0c361a4530ea0dcc7ffb16cc8c2a6bd8905793
tty007 <c7dc007@gmail.com>
2020-01-06 16:52:06 +0900

    windows用にpower取得ファイルを作成

--- testcode_power.py
+++ testcode_power.py
@@ -1,28 +1,31 @@
 import serial
 import sys
-import time
+import datetime
 
-h = serial.Serial("/dev/tty.Bluetooth-Incoming-Port",baudrate=9600,timeout=0.5)
-h.flushInput()
+# h = serial.Serial("/dev/tty.UM24C-Port",baudrate=9600,timeout=0.5)
+# win用
+h = serial.Serial("COM5",baudrate=9600,timeout=1.5)
 
-# h.write(b"\xf0")
-# rx = h.read(130)
-# print(rx)
+def getPowerInfo(h):
+  h.write(b"\xf0")
+  rx = h.read(130)
+  #raw data
+  # print(f'0x{rx[2]:02x}{rx[3]:02x}')
+  # print(f'0x{rx[4]:02x}{rx[5]:02x}')
+  # 0x01ea,0x010dのように表示される
+  v = float(rx[2]<<8|rx[3])/100.0
+  i = float(rx[4]<<8|rx[5])/1000.0
+  w = v * i
 
-# while True:
-#     time.sleep(1)
-#     h.write(bytes([0xf0]))
-#     s = h.read(33)
-#     print(sys.getsizeof(s))
-#     if (sys.getsizeof(s) < 33): continue
-#     print(s)
+  t_now = datetime.datetime.now().time()
+  print(t_now)
+  print(f'V:{v:.4f} [V]')
+  print(f'I:{i:.4f}[A]')
+  print(f'W:{w:.4f} [W]')
 
-buff = bytearray()
-
-while len(buff) < 130:
-    h.write(b"\xf0")
-    buff += h.readline()
-
-print(buff)
+i=1
+while i<10:
+  getPowerInfo(h)
+  i += 1
 
 h.close()
